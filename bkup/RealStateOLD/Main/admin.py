d72324060e4c8d05bcbdd70f50182f0a5bf51657
Gabo Araya <gabo.araya@gmail.com>
2022-10-07 01:35:10 -0300

    activa bkup + templates zip

---
+++ bkup/RealStateOLD/Main/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from Main.models import *
+
+# Register your models here.
+
+admin.site.register(Property)
+admin.site.register(Agent)
+admin.site.register(Customer)
+
+admin.site.register(Page)
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Image)
